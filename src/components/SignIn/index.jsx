6bfacfd1cf0be075f90e11df625b08dca4e0ac05
Daniel <daniel.albert.liu@gmail.com>
2023-12-31 22:55:30 -0500

    Completed 10.15

--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -1,5 +1,6 @@
 import { Formik } from 'formik';
 import * as yup from 'yup';
+import { useNavigate } from 'react-router-native';
 
 import SignInForm from './SignInForm';
 import useSignIn from '../../hooks/useSignIn';
@@ -16,13 +17,15 @@ const validationSchema = yup.object().shape({
 
 const SignIn = () => {
   const [signIn] = useSignIn();
+  const navigate = useNavigate();
 
   const onSubmit = async (values) => {
     const { username, password } = values;
 
     try {
-      const { data } = await signIn({ username, password });
+      const data = await signIn({ username, password });
       console.log(data);
+      navigate('/');
     } catch (e) {
       console.log(e);
     }
